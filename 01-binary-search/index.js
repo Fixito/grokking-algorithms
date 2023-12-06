34d92f02deb6f64befe8a62f7af5263da95a2800
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-12-06 20:33:00 +0100

    Transpile binary search algo to JS and fix bug

---
+++ 01-binary-search/index.js
@@ -0,0 +1,25 @@
+function binarySearch(list, item) {
+  let low = 0;
+  let high = list.length - 1;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const guess = list[mid];
+
+    if (guess === item) {
+      return mid;
+    }
+
+    if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return null;
+}
+
+myLIst = [1, 3, 5, 7, 9];
+
+console.log(binarySearch(myLIst, 3));
+console.log(binarySearch(myLIst, -1));
